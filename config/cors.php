3d6578e3d4b2b185bd8290d91cfd4fecbd4ccc0d
Alvino <alvinorino890@gmail.com>
2026-09-06 13:37:29 +0700

    Menambahkan file cors.php untuk API

--- config/cors.php
+++ config/cors.php
@@ -20,8 +20,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-    'http://localhost:5173', 
-    'http://localhost:3000',
+    'http://localhost:5173', // Alamat lokal React Shafaraz
+    'http://localhost:3000', // Alternatif lokal
+    'https://[link-vercel-lo-nanti].vercel.app' // Alamat live frontend
     ],
 
     'allowed_origins_patterns' => [],
